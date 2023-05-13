565ea67e80cb6119f2624794f1559107cf6c9660
Jesus Fernando Noy Burbano (Lider Técnico) <jfnoy@keralty.com>
2023-05-12 20:02:43 -0500

    ajuste en server para utilizar needle

--- server/server.js
+++ server/server.js
@@ -1,53 +1,31 @@
 const express = require('express')
-const api_helper = require('./API_helper')
-const app = express()
 const cors = require('cors')
+require('dotenv').config()
 
+//const api_helper = require('./API_helper')
+const errorHandler = require('./middleware/error')
+
+const PORT = process.env.PORT || 5000
+
+
+const app = express()
+
+
+// Enable cors
 app.use(cors())
 
-app.get('/api', (req, res) => {
-  res.send("GET Request Called")
-})
+// Routes
+app.use('/api', require('./routes/index'))
 
-app.get('/api/OrderByHemoglobin', (req, res) => {
-  console.log("Call API /OrderByHemoglobin")
-  api_helper.make_API_call('http://localhost:8085/Management/OrderByHemoglobin')
-  .then(response => {
-      res.json(response)
-  })
-  .catch(error => {
-      res.send(error)
-  })
-})
+app.use('/api', require('./routes/orderByHemoglobin'))
+
+app.use('/api', require('./routes/patientInfo'))
+
+app.use('/api', require('./routes/patientActivity'))
+
+// Error handler middleware
+app.use(errorHandler)
 
 
-app.get('/api/patientInfo/:id', (req, res) => {
-  console.log("Call API /patientInfo/:id")  
-  var patientId = req.params.id;
-  console.log(patientId)
-  api_helper.make_API_call('http://localhost:8085/patient/id/'+patientId)
-  .then(response => {
-      res.json(response)
-  })
-  .catch(error => {
-      res.send(error)
-  })
-})
-
-app.get('/api/patientActivity/:id', (req, res) => {
-  console.log("Call API /patientActivity/:id")
-  var patientId = req.params.id;
-  var patientId = req.params.id;
-  api_helper.make_API_call('http://localhost:8085/activity/ActivitiesByPatient/'+patientId)
-  .then(response => {
-      res.json(response)
-  })
-  .catch(error => {
-      res.send(error)
-  })
-})
-
-
-
-app.listen(5000, () => {console.log("Server started on port 5000")})
+app.listen(PORT, () => {console.log("Server started on port 5000")})
 
